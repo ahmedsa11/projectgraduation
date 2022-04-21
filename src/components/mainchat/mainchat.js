a829b61821aaa253c12702e20df29b285dabe0f4
ahmedsa11 <ahmedsa122333@gmail.com>
2022-04-21 21:10:52 +0200

    prepare the main chat

--- src/components/mainchat/mainchat.js
+++ src/components/mainchat/mainchat.js
@@ -4,7 +4,7 @@ import { Redirect } from "react-router";
 import Header from "../video conference/home/header";
 import Navbar from "../video conference/navbar/navbar";
 import i from "../../img/download.png"
-import Chathome from "../video conference/home/chathome";
+import chats from "../../img/download.png"
 const Mainchat = () => {
   const tempuser = localStorage.getItem("user");
   // console.log(tempuser)
@@ -22,11 +22,135 @@ const Mainchat = () => {
             <Navbar />
             <div className="vid-stream">
                 <div className="contacts">
-                    <Chathome/>
+                <div className="lst">
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                <div className="cont">
+                <div className="continfo"> <img src={chats} alt="a" />
+                <div>
+                  <span className="username">ahmed</span>
+                  <span className="usernumber">01066923650</span>
+                </div>
+                </div> 
+                  <div className="nummsg">
+                  <span>3</span></div>
+
+                </div>
+                </div>
                 </div>
                 <div className="chatarea">
                    <img src={i} alt="icon"/>
+                   <p>Now send and recieve message with<br/> your contacts</p>
                 </div>
+                
                 </div>
           </div>
           </div></div>
