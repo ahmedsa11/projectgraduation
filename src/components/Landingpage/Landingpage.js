1b0c45dd9da923bb1bfc99292b2560ff6f280094
ahmedsa11 <ahmedsa122333@gmail.com>
2022-05-12 13:19:39 +0200

    as

--- src/components/Landingpage/Landingpage.js
+++ src/components/Landingpage/Landingpage.js
@@ -3,6 +3,9 @@ import './landingpage.css'
 import logo from '../../img/log.png'
 import intro from '../../img/Girl with PC on zoom meeting 1.png'
 import icon8 from '../../img/icons8-closed-captioning-32.png'
+import googleplay from '../../img/googleplay.png'
+import imgapp from '../../img/image 28.png'
+import ahmedsalama from '../../img/salama.jpeg'
 const LandingPage = () => {
 //   const tempuser = localStorage.getItem("user");
 // //   let user = JSON.parse(tempuser);
@@ -40,6 +43,7 @@ const LandingPage = () => {
             </div>
             <div className="feature">
                 <h1>Check Out All Feature</h1>
+                <div class="colorbar1"></div>
               
                 <div className="row">
                     <div className="col-md-4">
@@ -114,11 +118,146 @@ const LandingPage = () => {
             </div>
 
 
+            <div className="work">
+                <h1>How does it work</h1>
+                <div class="colorbar1"></div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="workicon">
+                            <div className="infoo">
+                           <img src={icon8} alt="icon8"/>
+                           <h3>ahmed</h3>
+                           <p>sufdusgfussdf</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                        <div className="workicon">
+                        <div className="infoo">
+                           <img src={icon8} alt="icon8"/>
+                           <h3>ahmed</h3>
+                           <p>sufdusgfussdf</p>
+                        </div>
+                        </div>
+                        <div className="ly"></div>
+                        </div>
+                       
+                    </div>
+                    <div className="col-md-4">
+                        <div className="workicon">
+                        <div className="infoo">
+                           <img src={icon8} alt="icon8"/>
+                           <h3>ahmed</h3>
+                           <p>sufdusgfussdf</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div className="ourteam">
+                <h1>Meet Our Team</h1>
+                <div class="colorbar1"></div>
+                <div className="row">
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                            <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                                 <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                                 <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                                 <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                                 <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                                 <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                        <div className="featuerii">
+                                 <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6">
+                        <div className="ourteaminfo">
+                                 <div className="im">
+                           <img src={ahmedsalama} alt="icon8"/>
+                           </div>
+                           <h3>ahmed</h3>
+                           <span>asda</span>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+            <div className="android">
+                <div className="imgapp">
+                    <img src={imgapp} alt="app" />
+                </div>
+                <div className="textapp">
+                    <div className="textcontent">
+                    <h3>Download our app </h3>
+                    <h1>Get Connect App<br/> More Easily</h1>
+                    <p>asdjaoijdoajodjaojdajdojaodjaojs aij oaisjd oaj oaj oj jaoijsdaa</p>
+                    <img src={googleplay} alt="googleplay" />
+                    </div>
+                </div>
 
+            </div>
 
             <div className="footer" >
+                    
                 <div className="container">
+                
                     <div className="row">
                         <div class ="col-md-6">
                         <h2> <img src={logo}alt="as"/> Connect</h2>
