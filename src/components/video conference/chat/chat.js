c4a5eaf71c6733a6a0f87306b2485ad8c182dbf5
ahmedsa11 <ahmedsa122333@gmail.com>
2022-05-18 13:54:43 +0200

    input img

--- src/components/video conference/chat/chat.js
+++ src/components/video conference/chat/chat.js
@@ -5,14 +5,15 @@ import socket from '../socket';
 import { Redirect } from 'react-router';
 const Chat = ({ roomId }) => {
   const [msg, setMsg] = useState([]);
-  const [msgpic, setMsgpic] = useState();
+  const [inputImage, setinputImage] = useState('');
+  const [imageSended, setimageSended] = useState(false);
   const messagesEndRef = useRef(null);
   const inputRef = useRef();
 
   useEffect(() => {
-    socket.on('FE-receive-message', ({ msg, sender, img }) => {
+    socket.on('FE-receive-message', ({ msg, sender, img ,inputImage}) => {
       console.log({ msg, sender, img });
-      setMsg((msgs) => [...msgs, { sender, msg, img }]);
+      setMsg((msgs) => [...msgs, { sender, msg, img,inputImage }]);
     });
   }, []);
   const scrollToBottom = () => {
@@ -35,16 +36,19 @@ const Chat = ({ roomId }) => {
   // Scroll to Bottom of Message List
 
   const sendMessage = (e) => {
+    setimageSended(true);
     const text = document.getElementById('textt');
     const msg = text.value;
+
     // localStorage.setItem("text",text.value);
-    if (msg) {
-      console.log(msg);
-      console.log({ socket: socket });
-      console.log({ roomId, msg, sender: currentUser, img: imageuser });
+    if (msg || inputImage !== '') {
+      console.log(msg, inputImage);
+      // console.log({ socket: socket });
+      // console.log({ roomId, msg, sender: currentUser, img: imageuser });
       socket.emit('BE-send-message', {
         roomId,
         msg,
+        inputImage,
         sender: currentUser,
         img: imageuser,
       });
@@ -59,18 +63,28 @@ const Chat = ({ roomId }) => {
   // }
   function importData() {
     let input = document.createElement('input');
+
     input.type = 'file';
     input.accept = 'image/*';
     input.onchange = (e) => {
       let file = e.target.files[0];
-      const formData = new FormData();
-      formData.append('image', file, file.name);
-      setMsgpic(file);
-      console.log(file);
+      // const formData = new FormData();
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        // console.log(reader.result);
+        setinputImage(reader.result);
+      };
+      // formData.append('image', file, file.name);
+      // setMsgpic(file);
+      // // setImage(file);
+      // console.log(file);
+
       // const text = document.getElementById('textt');
     };
+
     input.click();
-    console.log(msgpic);
+    console.log(inputImage);
   }
   return (
     <react.Fragment>
@@ -78,15 +92,15 @@ const Chat = ({ roomId }) => {
         <i className="fas fa-sign-out-alt"></i>
         <div className="chat">
           {msg &&
-            msg.map(({ sender, msg, img }, idx) => {
+            msg.map(({ sender, msg, img ,inputImage}, idx) => {
               if (sender !== currentUser) {
                 return (
                   <div className="sender" key={idx}>
                     <img src={img} alt="a" />
-
                     <div className="text-box">
                       <strong>{sender} : </strong>
-                      <p> {msg}</p>
+                       {inputImage}
+                      <p>{msg} </p>
                       {/*<div className="message-time-left">SMS 13:37</div>*/}
                     </div>
                   </div>
@@ -96,6 +110,7 @@ const Chat = ({ roomId }) => {
                   <div className="reciver" key={idx}>
                     {/* <strong>{sender}</strong> */}
                     <div className="text-box">
+                      {inputImage}
                       <p>{msg} </p>
                       {/*<div className="message-time-right">SMS 13:37</div>*/}
                     </div>
@@ -116,7 +131,9 @@ const Chat = ({ roomId }) => {
               <i className="fas fa-image" id="imggg" onClick={importData}></i>
               {/* <i className="fas fa-microphone"></i> */}
             </div>
-
+            {inputImage !== '' && !imageSended && (
+              <img className="sended-image" src={inputImage} alt="fj"/>
+            )}
             <textarea
               type="text"
               placeholder="Write message...."
